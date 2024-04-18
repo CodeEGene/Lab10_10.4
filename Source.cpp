70353408c28f4076b32584f302d2a50858ccfbfd
ericschmidutz <e-schmidutz@onu.edu>
2024-04-17 23:45:19 -0400

    Copied in lab 5.4

--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,35 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
 int main() {
+	int cardNumber;
+	int digits[8];
+	int firstSum = 0;
+	int secondSum = 0;
+	int totalSum = firstSum + secondSum;
 
+	cout << "Please enter an 8-digit card number: ";
+	cin >> cardNumber;
 
-	return 0;
+	for (int i = 0; i < 8; i++) {
+		int digitPlace = pow(10, i + 1);
+		int tens = pow(10, i);
+		digits[i] = (cardNumber % digitPlace) / tens;
+
+		if (i % 2 == 0) {
+			firstSum += digits[i];
+		}
+		else {
+			secondSum += ((2 * digits[i]) % 10) + ((2 * digits[i]) % 100) / 10;
+		}
+	}
+	totalSum = firstSum + secondSum;
+
+	if ((totalSum % 10) == 0) {
+		cout << "Card is valid";
+	}
+	else {
+		cout << "Card is not vaild";
+	}
 }
