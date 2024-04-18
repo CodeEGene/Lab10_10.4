c7e1fba415fc9f0d1ce419524012681af4b1e194
ericschmidutz <e-schmidutz@onu.edu>
2024-04-18 01:22:17 -0400

    Done.

--- Source.cpp
+++ Source.cpp
@@ -12,19 +12,29 @@ bool isCorrectLength(string s, int size);
 int main() {
 	string cardNumber;
 	int digits[ARRAYSIZE];
-	int digitPlace = 0;
 	int tens = 0;
-	int intCardNumber;
+	int intCardNumber = 0;
 	
-	cout << "Enter an 8-digit card number or Q to quit: ";
+	cout << "Enter a 16-digit card number or Q to quit: ";
 	while (cin >> cardNumber) {
-		intCardNumber = stoi(cardNumber);
-		for (int i = 0; i < ARRAYSIZE; i++) {
-			digitPlace = pow(10, i + 1);
-			tens = pow(10, i);
-			digits[i] = (intCardNumber % digitPlace) / tens;
+		if (cardNumber == "Q" || cardNumber == "q") {
+			return 1;
 		}
-		
+		if (!isCorrectLength(cardNumber, ARRAYSIZE)) {
+			cout << endl << "Error - card number must contain 16 digits."<<endl;
+			cout << "Enter an 16-digit card number or Q to quit: ";
+			continue;
+		}
+		else if (!isNumeric(cardNumber)) {
+			cout << endl << "Error - card number must contain only digits." << endl;
+			cout << "Enter an 16-digit card number or Q to quit: ";
+			continue;
+		}
+
+
+		for (int i = 0; i < ARRAYSIZE; i++) {
+			digits[i] = cardNumber[i] - '0';
+		}
 
 		if (isCardValid(digits, ARRAYSIZE)) {
 			cout << "Card is valid."<<endl;
@@ -32,26 +42,27 @@ int main() {
 		else {
 			cout << "Card is not vaild." << endl;
 		}
-		cout << "Enter an 8-digit card number or Q to quit: ";
+		cout << "Enter an 16-digit card number or Q to quit: ";
 	}
+	return 0;
 }
 
 bool isCardValid(int digits[], int size) {
 	int firstSum = 0;
 	int secondSum = 0;
 	int totalSum = firstSum + secondSum;
+	int j = 0;
 
-	for (int i = 0; i < size; i++) {
-
+	for (int i = 0 ; i < size; i++) {
+		j = size - 1 - i;
 		if (i % 2 == 0) {
-			firstSum += digits[i];
+			firstSum += digits[j];
 		}
 		else {
-			secondSum += ((2 * digits[i]) % 10) + ((2 * digits[i]) % 100) / 10;
+			secondSum += ((2 * digits[j]) % 10) + ((2 * digits[j]) % 100) / 10;
 		}
 	}
 	totalSum = firstSum + secondSum;
-
 	if ((totalSum % 10) == 0) {
 		return true;
 	}
